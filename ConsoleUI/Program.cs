013bde77146f998bbb83269d481c2f32a7f89dd0
Derek Sparks <drsparks@gmail.com>
2020-04-23 19:52:53 -0700

    Initial Import

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is your first name: ");
+            string firstName = Console.ReadLine();
+
+            Console.Write("What is your last name?");
+            string lastName = Console.ReadLine();
+
+            if (firstName.ToLower() == "derek" && lastName.ToLower() == "sparks")
+            {
+                Console.WriteLine("Hello Professor Derek!");
+            }
+            else if (firstName.ToLower() == "sam" && lastName.ToLower() == "vongphonthong")
+            {
+                Console.WriteLine("Hello Sam");
+            }
+            else
+            {
+                Console.WriteLine("Hello student");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Application done!");
+
+            Console.ReadLine();
+        }
+    }
+}
